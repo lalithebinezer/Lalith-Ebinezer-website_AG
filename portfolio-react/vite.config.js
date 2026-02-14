f2cc4d5214bfe21c61d1805655fa66f244729263
lalithebinezer <lalithebinezer26@gmail.com>
2026-02-14 17:23:10 +0300

    feat: Initialize new React portfolio project with core components, global styling, and dark mode functionality.

--- portfolio-react/vite.config.js
+++ portfolio-react/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/website_AG/',
+  base: './',
   plugins: [react()],
   test: {
     globals: true,
